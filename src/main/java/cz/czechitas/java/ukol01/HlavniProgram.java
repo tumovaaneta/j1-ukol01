b53a58312549046387e37149a332525b4b46a977
Aneta <tumova-aneta@seznam.cz>
2023-02-17 19:51:12 +0100

    Prase

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -9,7 +9,8 @@ private Turtle zofka;
         nakresliPrasatko();
 
     }
-    private void nakresliNohyPrasatka() {
+    private void nakresliPrasatkoNohy() {
+//        vylepsit - funkce nakresliNohu,kterou 2xzavolam
         zofka.turnLeft(30);
         zofka.move(25);
         zofka.turnLeft(180);
@@ -21,33 +22,49 @@ private Turtle zofka;
         zofka.turnLeft(30);
     }
 
-    private void nakresliPrasatko()
-    {
+    private void nakresliPrasatkoTelo(){
         for (int i = 0; i < 2; i++) {
             zofka.move(50);
             zofka.turnRight(90);
             zofka.move(100);
             zofka.turnRight(90);
         }
-        zofka.turnLeft(60);
+    }
+
+    private void nakresliPrasatkoHlava(){
         zofka.move(50);
         for (int i = 0; i < 2; i++) {
             zofka.turnRight(120);
             zofka.move(50);
         }
-        nakresliNohyPrasatka();
-        zofka.turnRight(90);
-        zofka.move(100);
-        zofka.turnRight(90);
-        nakresliNohyPrasatka();
-        zofka.move(50);
+    }
+    private void nakresliPrasatkoOcasek(){
         for (int i = 0; i < 3; i++) {
             zofka.turnRight(60);
             zofka.move(15);
         }
-
-
     }
+    private void nakresliPrasatko() {
+        nakresliPrasatkoTelo();
+
+        zofka.turnLeft(60);
+
+        nakresliPrasatkoHlava();
+
+        nakresliPrasatkoNohy();
+
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(90);
+
+        nakresliPrasatkoNohy();
+        zofka.move(50);
+
+        nakresliPrasatkoOcasek();
+    }
+
+
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
